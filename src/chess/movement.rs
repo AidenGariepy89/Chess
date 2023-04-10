21061b8b42f6ec923ceee3eab67c91e68bcf2a50
AidenGariepy89 <79092752+AidenGariepy89@users.noreply.github.com>
2023-04-10 17:57:23 -0400

    Feat: pawn capturing

--- src/chess/movement.rs
+++ src/chess/movement.rs
@@ -52,6 +52,13 @@ fn pawn_movement(board: &Board, m: &Move, p: PlayerPiece) -> Result<()> {
         Player::White => {
             if m.to > m.from { return Err(anyhow!("Pawn cannot move backwards!")); }
 
+            if let Piece::Piece(_) = board.get_space(m.to).unwrap() {
+                if m.to == m.from - ROW_LEN + 1 || m.to == m.from - ROW_LEN - 1 {
+                    return Ok(());
+                }
+                return Err(anyhow!("Pawn can only capture diagonally one space ahead!"));
+            }
+
             if m.from >= 48 && m.from < 56 {
                 if m.from - m.to == ROW_LEN || m.from - m.to == ROW_LEN * 2 {
                     return Ok(());
